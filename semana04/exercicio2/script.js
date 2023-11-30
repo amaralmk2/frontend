5c7dc1bbc8c319a524824c1849219d411f252b17
Alexandre Amaral <alexandreamaral.gt@gmail.com>
2023-11-30 11:53:42 -0300

    Feito parte do exercicio 2 da aula 4 de front

---
+++ semana04/exercicio2/script.js
@@ -0,0 +1,18 @@
+class Computador {
+    constructor(marca, modelo, processador, placamae){
+        marca = marca
+        modelo = modelo
+        processador = processador
+        placamae = placamae
+    }
+
+}
+
+ let computador1 = new Computador()
+ let computador2 = new Computador()
+ let computador3 = new Computador()
+
+ function getObject(computador1) {
+    
+ }
+
