38c5706f18be5a44689c116ae5f43e7e847838a3
Alexandre Amaral <alexandreamaral.gt@gmail.com>
2023-12-06 17:22:44 -0300

    criado metodo em JS que cria JSON e impreme em terminal criado base de HTML para formulario

--- PI004/script.js
+++ PI004/script.js
@@ -2,27 +2,27 @@ document.addEventListener('DOMContentLoaded', function() {
     // Adiciona um ouvinte de evento para cada botão "Comprar"
     var buttons = document.querySelectorAll('.comprar-btn');
     buttons.forEach(function(button) {
-        button.addEventListener('click', capturarInformacoes);
+        button.addEventListener('click', capturaTeclado);
     });
 });
 
-function capturarInformacoes(event) {
+function capturaTeclado(event) {
 
     const card = event.target.closest('.card');
 
     const destino = card.querySelector('h3').textContent;
     const preco = parseFloat(card.querySelector('h2').textContent.replace('R$ ', '').replace(',', '.'));
     const lists = card.querySelectorAll('li');
-    const listValues = Array.from(lists).map(li => li.textContent);
+    const listaValores = Array.from(lists).map(li => li.textContent);
     const spans = card.querySelectorAll('span');
     const spanValues = Array.from(spans).map(span => span.textContent);
 
-    const pacoteTuristico = {
+    const Pacote = {
         destino: destino,
-        lista: listValues,
+        lista: listaValores,
         preco: preco,
         spanValues
     };
 
-    console.log(pacoteTuristico);
+    console.log(Pacote);
 }
